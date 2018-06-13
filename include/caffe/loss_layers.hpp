3c663adba351f7aa90ee1941149a10c1783df8e4
cad <shaoxinxiaosai@163.com>
2018-06-13 11:03:48 +0800

    add huber loss

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -994,7 +994,55 @@ namespace caffe {
         // sum of weights
         Dtype weights_sum_;
     };
-  
+
+    /**
+     * @brief Computes the SmoothL1 loss as introduced in:@f$
+     *  Fast R-CNN, Ross Girshick, ICCV 2015.
+     */
+    template <typename Dtype>
+    class SmoothL1LossLayer : public LossLayer<Dtype> {
+     public:
+      explicit SmoothL1LossLayer(const LayerParameter& param)
+          : LossLayer<Dtype>(param), diff_() {}
+      virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+          const vector<Blob<Dtype>*>& top);
+      virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+          const vector<Blob<Dtype>*>& top);
+
+      virtual inline LayerParameter_LayerType type()
+        const { return LayerParameter_LayerType_SMOOTH_L1_LOSS;}
+
+      virtual inline int ExactNumBottomBlobs() const { return -1; }
+      virtual inline int MinBottomBlobs() const { return 2; }
+      virtual inline int MaxBottomBlobs() const { return 4; }
+
+      /**
+       * Unlike most loss layers, in the SmoothL1LossLayer we can backpropagate
+       * to both inputs -- override to return true and always allow force_backward.
+       */
+      virtual inline bool AllowForceBackward(const int bottom_index) const {
+        return true;
+      }
+
+     protected:
+      /// @copydoc SmoothL1LossLayer
+      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+          const vector<Blob<Dtype>*>& top);
+      //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      //    const vector<Blob<Dtype>*>& top);
+
+      virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+          const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+     // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+      Blob<Dtype> diff_;
+      Blob<Dtype> errors_;
+      Blob<Dtype> ones_;
+      bool has_weights_;
+      Dtype sigma2_;
+    };
+
     /**
      * Author: Shi Neng
      * @brief change of softmax loss view layer, use only one kind of class, split the sphere
